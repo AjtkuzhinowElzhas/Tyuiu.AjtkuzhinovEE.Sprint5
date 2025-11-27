489c413debce1e991c9e3e2f05ede8ed647bd20e
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-11-28 00:25:26 +0300

    Task6

---
+++ Tyuiu.AjtkuzhinovEE.Sprint5.Task6.V30.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint5.Task6.V30.Lib
+{
+    public class DataService : ISprint5Task6V30
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Разбиваем строку на слова и сразу проверяем каждое
+                    int start = 0;
+                    for (int i = 0; i <= line.Length; i++)
+                    {
+                        // Если достигли пробела или конца строки
+                        if (i == line.Length || line[i] == ' ')
+                        {
+                            // Проверяем длину слова
+                            if (i - start == 8)
+                            {
+                                count++;
+                            }
+                            start = i + 1; // Начало следующего слова
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.AjtkuzhinovEE.Sprint5.Task6.V30.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint5.Task6.V30.Lib;
+using System.IO;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint5.Task6.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        DataService ds = new DataService();
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V30.txt";
+            FileInfo fileInfo = new FileInfo(path);
+
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+        [TestMethod]
+        public void ValidCalc()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V30.txt";
+
+            var res = ds.LoadFromDataFile(path);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+
+    }
+}
