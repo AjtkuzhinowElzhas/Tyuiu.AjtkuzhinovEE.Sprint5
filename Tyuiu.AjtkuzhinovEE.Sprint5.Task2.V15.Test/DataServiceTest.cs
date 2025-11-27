8c924c70ef033abaea366a73be3a165b4bbef6d9
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-11-27 17:29:45 +0300

    Task2

---
+++ Tyuiu.AjtkuzhinovEE.Sprint5.Task2.V15.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Tyuiu.AjtkuzhinovEE.Sprint5.Task2.V15.Lib;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint5.Task2.V15.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = $"C:\\Users\\user\\AppData\\Local\\Temp\\OutPutFileTask2.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.AjtkuzhinovEE.Sprint5.Task2.V15/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.AjtkuzhinovEE.Sprint5.Task2.V15.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[,] matrix = new int[3, 3] { { 6, 1, 7},
+                                        { 1, 8, 5},
+                                         {6, 6, 4}      };
+        int rows = matrix.GetUpperBound(0) + 1;
+        int columns = matrix.Length / rows;
+
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Массив:");
+        for(int i = 0; i < rows; i++)
+        {
+            for(int j = 0; j < columns; j++)
+            {
+                Console.WriteLine($"{matrix[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        string res = ds.SaveToFileTextData(matrix);
+
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+
+    }
+}
