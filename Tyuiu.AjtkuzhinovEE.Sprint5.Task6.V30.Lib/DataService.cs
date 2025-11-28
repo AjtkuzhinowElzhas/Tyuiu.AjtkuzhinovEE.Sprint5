bd1b8aa26b158079c2e554a0dfab011b8e41724b
AjtkuzhinovElzhas <aitkuzinow8@gmail.com>
2025-11-28 18:47:13 +0300

    Task6

---
+++ Tyuiu.AjtkuzhinovEE.Sprint5.Task6.V30.Lib/DataService.cs
@@ -0,0 +1,35 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+
+namespace Tyuiu.AjtkuzhinovEE.Sprint5.Task6.V30.Lib;
+
+public class DataService : ISprint5Task6V30
+{
+    public int LoadFromDataFile(string path)
+    {
+        int count = 0;
+        using (StreamReader reader = new StreamReader(path))
+        {
+            string line;
+
+            while ((line = reader.ReadLine()) != null)
+            {
+
+                int start = 0;
+                for (int i = 0; i <= line.Length; i++)
+                {
+                    if (i == line.Length || line[i] == ' ')
+                    {
+
+                        if (i - start == 8)
+                        {
+                            count++;
+                        }
+                        start = i + 1;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
---
+++ Tyuiu.AjtkuzhinovEE.Sprint5.Task6.V30/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Tyuiu.AjtkuzhinovEE.Sprint5.Task6.V30.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫНЕ:                                                       *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\DataSprint5\InPutDataFileTask6V30.txt";
+
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine(res);
+
+
+        Console.ReadKey();
+    }
+}
